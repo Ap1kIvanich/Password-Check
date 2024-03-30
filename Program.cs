de173c975b6bc372410a30a1c5914af044c244cc
Роман Букатов <156239298+Ap1kIvanich@users.noreply.github.com>
2024-03-31 06:40:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace PasswordProgram
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите пароль: ");
+            string TruePassword = ReadAllText();
+            string InputPassword = Console.ReadLine();
+
+
+            
+            if(InputPassword == TruePassword) 
+            {
+                Access();
+                
+            }
+
+            else 
+            {
+                Decline();
+            }
+            
+        }
+
+        static string ReadAllText(string path = @"D:\VisualStudio\PasswordProgram\password.txt")
+        {
+            string password = File.ReadAllText(path);
+            return password;
+        }
+        static void Access() 
+        {
+            Console.WriteLine("Доступ!");
+        }
+
+        static void Decline() 
+        {
+            Console.WriteLine("Пароль введён неверно.");
+        }
+    }
+}
